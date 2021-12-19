dabe94ebf5751f8f915c1881ff455f80df53ad76
JRIGS <raczkowskijonathan@gmail.com>
2021-12-18 20:22:27 -0500

    Basic Commands

--- src/bot.js
+++ src/bot.js
@@ -1,16 +1,45 @@
 require('dotenv').config();
 
-const { Client, Intents } = require('discord.js');
+const { Client, Intents, Guild } = require('discord.js');
 const client = new Client({ intents: [Intents.FLAGS.GUILDS,Intents.FLAGS.GUILD_MESSAGES] });
+const PREFIX = "$";
 
-//initializing bot to a ready status
+//confirm ready status from bot
 client.on('ready', () => {
     console.log(`Bot ${client.user.tag} is online!`);
 });
 
 // If a message is created by a user then log it
 client.on('messageCreate', (message) => {
-    console.log(message.content);
-})
+    if (message.author.bot) return;
+//Prefix conditional    
+    if (message.content.startsWith(PREFIX)) {
+        const [CMD_NAME, ...args] = message.content
+            .trim()
+            .substring(PREFIX.length)
+            .split(/\s+/);
+        // console.log(CMD_NAME);
+        // console.log(args);
+
+        if (CMD_NAME === 'kick') {
+            if (args.length === 0) return message.reply('Please provide an ID');
+            const member = message.guild.members.cache.get(args[0]);
+            if (member) {
+                member.kick();
+                // console.log(`${member.author.tag} kicked!`);
+            } else {
+                message.channel.send('That member was not found')
+            }
+        } 
+        // else if (CMD_NAME === 'ban') {
+        //     message.channel.sent('Banned the user');
+        // }
+        
+    }
+    console.log(`[${message.author.tag}]: ${message.content}`);
+    if (message.content.toLowerCase() === 'hello') {
+        message.reply('Hello there!');
+    }
+});
 
 client.login(process.env.DISCORDJS_BOT_TOKEN);
